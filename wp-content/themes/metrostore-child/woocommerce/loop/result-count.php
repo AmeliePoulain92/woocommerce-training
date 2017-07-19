ac99876478de73b3ca2f5fed8a0c211ee8df5ae8
Vocter <ameliepoulain-92@mail.ru>
2017-07-19 20:52:43 +0300

    translate results

--- wp-content/themes/metrostore-child/woocommerce/loop/result-count.php
+++ wp-content/themes/metrostore-child/woocommerce/loop/result-count.php
@@ -38,10 +38,10 @@ if ( ! woocommerce_products_will_display() ) {
 
 	if ( $total <= $per_page || -1 === $per_page ) {
 		/* translators: %d: total results */
-		printf( _n( 'Showing the single result', 'Showing all %d results', $total, 'woocommerce' ), $total );
+		printf( _n( 'Показан 1 результат', 'Показаны все %d результатов', $total, 'woocommerce' ), $total );
 	} else {
 		/* translators: 1: first result 2: last result 3: total results */
-		printf( _nx( 'Showing the single result', 'Showing %1$d&ndash;%2$d of %3$d results', $total, 'with first and last result', 'woocommerce' ), $first, $last, $total );
+		printf( _nx( 'Показан 1 результат', 'Показаны %1$d&ndash;%2$d в %3$d результатах', $total, 'с первым и последним результатом', 'woocommerce' ), $first, $last, $total );
 	}
 	?>
 </p>
